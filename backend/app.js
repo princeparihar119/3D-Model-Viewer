e443e5d78fc06a099b4796ff20b4d345d13bd5ac
princeparihar119 <Princeparihar7898@gmail.com>
2026-08-22 15:03:14 +0530

    setup app.js to vercel

--- backend/app.js
+++ backend/app.js
@@ -6,12 +6,32 @@ import authRoutes from "./routes/authRoutes.js";
 import modelRoutes from "./routes/modelRoutes.js";
 
 const app = express();
+
 app.use(express.json());
 app.use(cookieParser());
 
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL || "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://3-d-model-viewer-murex.vercel.app",
+  process.env.FRONTEND_URL,
+].filter(Boolean);
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
     credentials: true,
   })
 );
@@ -20,7 +40,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/models", modelRoutes);
 
 app.get("/", (req, res) => {
-  res.json({
+  res.status(200).json({
     message: "3D Model Viewer API is running",
   });
 });
